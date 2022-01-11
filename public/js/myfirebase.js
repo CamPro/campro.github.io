d36d7367f16f484d92c98de47065caa146ca885d
cam <campxpro@gmail.com>
2022-01-11 16:44:08 +0700

    1

--- public/js/myfirebase.js
+++ public/js/myfirebase.js
@@ -186,7 +186,7 @@ function KnowledgeSave(klgIndex, learn, course, image = '')
 }
 
 function myClock() {
-	let days = ["Chủ Nhật", "Thứ 2", "Thứ 3", "Thứ 4", "Thứ 5", "Thứ 6", "Thứ 7"];
+	let days = ["CN", "T2", "T3", "T4", "T5", "T6", "T7"];
 	let d = new Date();
 	let day = days[d.getDay()];
 	let hr = d.getHours();
